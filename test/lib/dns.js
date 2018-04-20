8cc4fb793e4103ead1cc2fecc5b778b26d37dbae
Makoto <inouemak@googlemail.com>
2018-04-20 06:01:19 +0100

    Make interface of encodeTypeBitMap similar to the rest

--- test/lib/dns.js
+++ test/lib/dns.js
@@ -1,22 +1,36 @@
 var dns = require("../../lib/dns.js");
 contract('DNSSEC', function(accounts) {
-    describe('encodeTypeBitMap', function(){
+    describe.only('encodeTypeBitMap', function(){
+        let buffer, off, result;
+
+        beforeEach(async function(){
+            buffer = new Buffer(10);
+            off = 0;
+        })
+
         it('encodes single type', function(){
-            assert.equal(
-                dns.encodeTypeBitMap([16]).toString('hex'), '0003000001'
-            );
+            result = dns.encodeTypeBitMap(buffer, off, [16]);
+            assert.equal(result, 5);
+            assert.equal(buffer.toString('hex'), '00030000010000000000');
+        });
+
+        it('encodes with offset', function(){
+            off = 1;
+            result = dns.encodeTypeBitMap(buffer, off, [16]);
+            assert.equal(result, 5);
+            assert.equal(buffer.toString('hex'), '00000300000100000000');
         });
 
         it('adds up types in the same bigmap segment', function(){
-            assert.equal(
-                dns.encodeTypeBitMap([16, 48]).toString('hex'), '000700000100000001'
-            );
+            result = dns.encodeTypeBitMap(buffer, off, [16, 48]);
+            assert.equal(result, 9);
+            assert.equal(buffer.toString('hex'), '00070000010000000100');
         });
 
         it('encodes across multiple windows', function(){
-            assert.equal(
-                dns.encodeTypeBitMap([256 + 16, 256 + 48]).toString('hex'), '010700000100000001'
-            );
+            result = dns.encodeTypeBitMap(buffer, off, [256 + 16, 256 + 48]);
+            assert.equal(result, 9);
+            assert.equal(buffer.toString('hex'), '01070000010000000100');
         });
     })
 })
