68e0901c2008660084a233713445238cb2368777
Makoto <inouemak@googlemail.com>
2018-04-26 15:06:49 +0100

    Remove duplicate tests

--- test/TestRRUtils.sol
+++ test/TestRRUtils.sol
@@ -43,16 +43,4 @@ contract TestRRUtils {
     Assert.equal(ethLabXyz.compareLabel(bthLabXyz) >  0, true, "ethLab.xyz comes after bethLab.xyz");
     Assert.equal(bthLabXyz.compareLabel(xyz)       >  0, true, "bthLab.xyz comes after xyz");
   }
-
-  function testCompareLabel() public {
-    bytes memory bthLabXyz = hex'066274686c61620378797a00';
-    bytes memory ethLabXyz = hex'066574686c61620378797a00';
-    bytes memory xyz = hex'0378797a00';
-    // Exists in bitmap
-    Assert.equal(xyz.compareLabel(ethLabXyz)       <  0, true, "xyz comes before ethLab.xyz");
-    Assert.equal(bthLabXyz.compareLabel(ethLabXyz) <  0, true, "bthLab.xyz comes before ethLab.xyz");
-    Assert.equal(bthLabXyz.compareLabel(bthLabXyz) == 0, true, "bthLab.xyz and bthLab.xyz are the same");
-    Assert.equal(ethLabXyz.compareLabel(bthLabXyz) >  0, true, "ethLab.xyz comes after bethLab.xyz");
-    Assert.equal(bthLabXyz.compareLabel(xyz)       >  0, true, "bthLab.xyz comes after xyz");
-  }
 }
