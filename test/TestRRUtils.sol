7d522305c0b67764dc45a43dd6723ce448dc02c8
Makoto <inouemak@googlemail.com>
2018-05-02 20:04:21 +0100

    Adde extra test

--- test/TestRRUtils.sol
+++ test/TestRRUtils.sol
@@ -81,12 +81,14 @@ contract TestRRUtils {
     bytes memory a_b_c  = hex'01610162016300';
     bytes memory b_b_c  = hex'01620162016300';
     bytes memory c      = hex'016300';
+    bytes memory d      = hex'016400';
     bytes memory a_d_c  = hex'01610164016300';
     bytes memory b_a_c  = hex'01620161016300';
     bytes memory ab_c_d = hex'0261620163016400';
     bytes memory a_c_d  = hex'01610163016400';
 
     Assert.equal(a_b_c.compareNames(c)      >  0, true, "one name has a difference of >1 label to the other");
+    Assert.equal(a_b_c.compareNames(d)      <  0, true, "one name has a difference of >1 label to the other");
     Assert.equal(a_b_c.compareNames(a_d_c)  <  0, true, "two names start the same but have differences in later labels");
     Assert.equal(a_b_c.compareNames(b_a_c)  >  0, true, "the first label sorts later, but the first label sorts earlier");
     Assert.equal(ab_c_d.compareNames(a_c_d) >  0, true, "two names where the first label on one is a prefix of the first label on the other");
