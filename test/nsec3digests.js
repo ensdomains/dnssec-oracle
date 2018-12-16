4f4794acf2188e491360725a50f010dc5c589a94
decanus <dean.eigenmann@icloud.com>
2018-12-16 21:41:35 +0700

    nsec indentation

--- test/nsec3digests.js
+++ test/nsec3digests.js
@@ -6,22 +6,22 @@ function fromBase32(s) {
 }
 
 vectors = [
-  ["", "", 0, "0xda39a3ee5e6b4b0d3255bfef95601890afd80709000000000000000000000000"],
-  ["nacl", "test", 0, "0x68b36a28941caebfc2af818c99a8e34478d77fec000000000000000000000000"],
-  ["nacl", "test", 1, "0x16574cbb9312cf064794482fdd1148289027db73000000000000000000000000"],
-  ["nacl", "test", 10, "0x455370ef51d39be8efa646b807a818c7649a505e000000000000000000000000"],
-  ["0xaabbccdd", "\x07example\x00", 12, fromBase32("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom")],
-  ["0xaabbccdd", "\x01a\x07example\x00", 12, fromBase32("35mthgpgcu1qg68fab165klnsnk3dpvl")],
-  ["0x5BA6AD4385844262", "\x07matoken\x03xyz\x00", 1, fromBase32("bst4hlje7r0o8c8p4o8q582lm0ejmiqt")]
+    ["", "", 0, "0xda39a3ee5e6b4b0d3255bfef95601890afd80709000000000000000000000000"],
+    ["nacl", "test", 0, "0x68b36a28941caebfc2af818c99a8e34478d77fec000000000000000000000000"],
+    ["nacl", "test", 1, "0x16574cbb9312cf064794482fdd1148289027db73000000000000000000000000"],
+    ["nacl", "test", 10, "0x455370ef51d39be8efa646b807a818c7649a505e000000000000000000000000"],
+    ["0xaabbccdd", "\x07example\x00", 12, fromBase32("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom")],
+    ["0xaabbccdd", "\x01a\x07example\x00", 12, fromBase32("35mthgpgcu1qg68fab165klnsnk3dpvl")],
+    ["0x5BA6AD4385844262", "\x07matoken\x03xyz\x00", 1, fromBase32("bst4hlje7r0o8c8p4o8q582lm0ejmiqt")]
 ];
 
 contract("SHA1NSEC3Digest", function(accounts) {
-  for(var i = 0; i < vectors.length; i++) {
+for(var i = 0; i < vectors.length; i++) {
     (function(i, vector) {
-      it('calculates test vector ' + i, async function() {
-        var instance = await sha1.deployed();
-        assert.equal(await instance.hash(vector[0], vector[1], vector[2]), vector[3])
-      });
+        it('calculates test vector ' + i, async function() {
+            var instance = await sha1.deployed();
+            assert.equal(await instance.hash(vector[0], vector[1], vector[2]), vector[3])
+        });
     })(i, vectors[i]);
-  }
+    }
 })
