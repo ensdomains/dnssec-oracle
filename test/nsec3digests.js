d2488866d5be8d55c8411ae8ca01c7beee451b96
decanus <dean.eigenmann@icloud.com>
2018-12-16 21:42:54 +0700

    fix

--- test/nsec3digests.js
+++ test/nsec3digests.js
@@ -16,12 +16,12 @@ vectors = [
 ];
 
 contract("SHA1NSEC3Digest", function(accounts) {
-for(var i = 0; i < vectors.length; i++) {
-    (function(i, vector) {
-        it('calculates test vector ' + i, async function() {
-            var instance = await sha1.deployed();
-            assert.equal(await instance.hash(vector[0], vector[1], vector[2]), vector[3])
-        });
-    })(i, vectors[i]);
+    for(var i = 0; i < vectors.length; i++) {
+        (function(i, vector) {
+            it('calculates test vector ' + i, async function() {
+                var instance = await sha1.deployed();
+                assert.equal(await instance.hash(vector[0], vector[1], vector[2]), vector[3])
+            });
+        })(i, vectors[i]);
     }
 })
