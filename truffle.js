bdb6ad8f03ed6262edce1b9e98eee5161a10008a
Makoto <inouemak@googlemail.com>
2019-09-17 17:18:47 +0100

    Add endpoint for the test

--- truffle.js
+++ truffle.js
@@ -1,8 +1,26 @@
 module.exports = {
   networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 4712388
+    },
     local: {
       host: 'localhost',
-      port: 9545,
+      port: 8545,
+      network_id: '*',
+      gas: 4712388
+    },
+    test: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 4712388
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
       network_id: '*',
       gas: 4712388
     },
