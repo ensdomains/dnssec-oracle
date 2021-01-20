ba09d85e803c518728586382471911b4610a3fa4
Nick Johnson <arachnid@notdot.net>
2021-01-21 12:36:42 +1300

    Make deployment easier

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
   networks: {
     local: {
@@ -7,11 +10,14 @@ module.exports = {
       gas: 4712388
     },
     ropsten: {
-      host: 'localhost',
-      port: 8545,
+      provider: function() {
+        return new HDWalletProvider([process.env.ROPSTEN_KEY], "https://ropsten.infura.io/v3/58a380d3ecd545b2b5b3dad5d2b18bf0");
+      },
+      // host: 'localhost',
+      // port: 8545,
       network_id: '3',
-      from: '0xa303ddc620aa7d1390baccc8a495508b183fab59',
-      gas: 5000000
+      from: '0xa303ddc620aa7d1390baccc8a495508b183fab59'
+      // gas: 5000000
     }
   },
   mocha: {
@@ -25,5 +31,11 @@ module.exports = {
     solc: {
       version: "0.7.4",
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
   }
 };
